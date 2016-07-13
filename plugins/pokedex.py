5f4099abbd24e3e3f8dc6053feea47406d3b1ab3
Miguel Arguelles <cmarguel@gmail.com>
2016-07-13 23:21:11 +0800

    Fix Nidoran issues

--- plugins/pokedex.py
+++ plugins/pokedex.py
@@ -21,7 +21,7 @@ def cache(db, id, name, language, version, flavor):
 
 
 def get_flavors(db, query, language):
-    return db.execute("select version, flavor from pokedex where "
+    return db.execute("select id, name, version, flavor from pokedex where "
                       "(id=? or name=? collate nocase) and "
                       "language=?",
                      (query, query, language)).fetchall()
@@ -42,11 +42,11 @@ def pokedex(inp, nick='', chan='', db=None, input=None):
     default_language = "en"
     default_version = "x"
 
-    query = inp.lower()
+    query = asciify(inp.lower())
     valid = re.match('^[\w-]+$', query) is not None
 
     if not valid:
-        return "Missingno"
+        return "A wild Missingno appeared!"
 
     requested_version = default_version
     requested_language = default_language
@@ -61,7 +61,7 @@ def pokedex(inp, nick='', chan='', db=None, input=None):
 
         return get_random_flavor(db, query, requested_language)
     except http.HTTPError:
-        return "Missingno"
+        return "A wild Missingno appeared!"
 
 
 def get_random_flavor(db, query, language):
@@ -69,35 +69,44 @@ def get_random_flavor(db, query, language):
     if flavors:
         rand = random.randint(0, len(flavors) - 1)
 
-        return flavors[rand][1]
+        prelude = "#%s %s - " % (flavors[rand][0], flavors[rand][1])
+        post = " (%s)" % flavors[rand][2]
+
+        return prelude + flavors[rand][3] + post
     else:
         return None
 
 
 def cache_flavors(db, entry, requested_language, requested_version):
     entries = entry['flavor_text_entries']
-    name = extract_name(entry, requested_language)
+    # name = extract_name(entry, requested_language)
     for flavor_entry in entries:
         language = flavor_entry["language"]["name"]
         version = flavor_entry["version"]["name"]
         if requested_language == language:
-            flavor = flavor_entry["flavor_text"].encode('utf-8').strip()
-            flavor = flavor.replace('\xa9', '')
-            flavor = flavor.replace('\xe9', 'e')
-            flavor = flavor.replace('\xc3', 'e')
-            flavor = flavor.replace('\xad', '')
-            flavor = flavor.replace('\f', ' ')
-            flavor = flavor.replace('\n', ' ')
-            flavor = flavor.replace('\u2019', '\'')
-            filtered = ""
-            for c in flavor:
-                if ord(c) < 128:
-                    filtered += c
-
-            flavor = filtered.encode('ascii')
-            cache(db, entry["id"], name, language, version, flavor)
+            flavor = asciify(flavor_entry["flavor_text"])
+            cache(db, entry["id"], entry["name"], language, version, flavor)
             # return flavor
-    return "Missingno"
+    return "A wild Missingno appeared!"
+
+
+def asciify(s):
+    flavor = s.encode('utf-8').strip()
+    flavor = flavor.replace('\xa9', '')
+    flavor = flavor.replace('\xe9', 'e')
+    flavor = flavor.replace('\xc3', 'e')
+    flavor = flavor.replace('\xad', '')
+    flavor = flavor.replace('\f', ' ')
+    flavor = flavor.replace('\n', ' ')
+    flavor = flavor.replace(u'\u2019', '\'')
+    flavor = flavor.replace(u'\u2642', '(m)')
+    flavor = flavor.replace(u'\u2640', '(f)')
+    filtered = ""
+    for c in flavor:
+        if ord(c) < 128:
+            filtered += c
+
+    return filtered.encode('ascii')
 
 
 def extract_name(entry, requested_language):
@@ -105,5 +114,5 @@ def extract_name(entry, requested_language):
     for name in names:
         language = name["language"]["name"]
         if language == requested_language:
-            return name["name"]
-    return "Missingno"
+            return asciify(name["name"])
+    return "A wild Missingno appeared!"
--- tests/pokedex_test.py
+++ tests/pokedex_test.py
@@ -9,23 +9,27 @@ class PokedexTest(plugin_test.PluginTest):
         plugin_test.PluginTest.setUp(self, plugins.pokedex)
 
     # TODO: Now that it returns a random dex entry, this test doesn't work
-    def test_pokedex_basic(self):
+    def xtest_pokedex_basic(self):
         nick("Ash").says(".pokedex pikachu")
-        #self.shouldSay(
-        #    "Ash: It raises its tail to check its surroundings."
-        #    " The tail is sometimes struck by lightning in this pose.")
+        self.shouldSay(
+            "Ash: It raises its tail to check its surroundings."
+            " The tail is sometimes struck by lightning in this pose.")
 
     def test_pokedex_not_found(self):
         nick("Ash").says(".pokedex missingno")
-        self.shouldSay("Ash: Missingno")
+        self.shouldSay("Ash: A wild Missingno appeared!")
 
     def test_invalid_pokemon(self):
         nick("Ash").says(".pokedex pika pika pi")
-        self.shouldSay("Ash: Missingno")
+        self.shouldSay("Ash: A wild Missingno appeared!")
 
-    def x_test_caching(self):
+    def test_lookup_by_id(self):
+        nick("Ash").says(".dex 1")
+        self.shouldSay("Ash: #1 Bulbasaur")
+
+    def xtest_caching(self):
         print "Ash is asking now."
-        nick("Ash").says(".pokedex pikachu")
+        nick("Ash").says(".pokedex nidoran-m")
         print "Brock is asking now."
-        nick("Brock").says(".pokedex pikachu")
+        nick("Brock").says(".pokedex nidoran-m")
         print "Done."
